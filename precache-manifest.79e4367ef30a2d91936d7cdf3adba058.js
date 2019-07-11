4f7fe56bc168f47ffa5395925a5f8279f5b399ee
Michael Ignacio <mikelignacio@gmail.com>
2019-07-11 12:47:48 +0800

    Updates

---
+++ precache-manifest.79e4367ef30a2d91936d7cdf3adba058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40a81ff4feffeea0da7957d2ed9cfbc",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "ef374ea39d6163722f06",
+    "url": "/markdown-previewer/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "8bef83fa3de23113d06c",
+    "url": "/markdown-previewer/static/js/2.5edc177a.chunk.js"
+  },
+  {
+    "revision": "ef374ea39d6163722f06",
+    "url": "/markdown-previewer/static/js/main.b4c9a607.chunk.js"
+  },
+  {
+    "revision": "6ac31481741a1aafa92f",
+    "url": "/markdown-previewer/static/js/runtime~main.2b49566a.js"
+  }
+]);
